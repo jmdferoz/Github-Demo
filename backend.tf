94c1b4519bcf8bc8b2374108ab7832d48d660712
jmdferoz <55978134+jmdferoz@users.noreply.github.com>
2025-05-15 19:26:13 +0530

    Create backend.tf

---
+++ backend.tf
@@ -0,0 +1,5 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "myResourceGroup"        # Same RG you created
+    storage_account_name = "tfstatestorage123fr"      # Must exist already
+    container_name       = "_
