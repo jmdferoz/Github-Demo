47a623c723f06d2f04619625265d442dbc684c71
jmdferoz <55978134+jmdferoz@users.noreply.github.com>
2025-05-15 19:27:14 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,4 +2,7 @@ terraform {
   backend "azurerm" {
     resource_group_name  = "myResourceGroup"        # Same RG you created
     storage_account_name = "tfstatestorage123fr"      # Must exist already
-    container_name       = "_
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
+}
